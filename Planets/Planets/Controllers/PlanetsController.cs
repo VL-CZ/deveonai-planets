40170ac1a9928c6912347b1f3c24c1809cc4f288
Vojtěch Lengál <vojta.lengal@gmail.com>
2025-10-16 21:10:14 +0200

    planets controller

---
+++ Planets/Planets/Controllers/PlanetsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Planets.Data;
+using Planets.Models;
+
+namespace Planets.Controllers
+{
+    public class PlanetsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PlanetsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Planets
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Planets.ToListAsync());
+        }
+
+        // GET: Planets/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var planet = await _context.Planets
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (planet == null)
+            {
+                return NotFound();
+            }
+
+            return View(planet);
+        }
+
+        // GET: Planets/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Planets/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Planet planet)
+        {
+            if (ModelState.IsValid)
+            {
+                planet.Id = Guid.NewGuid();
+                _context.Add(planet);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(planet);
+        }
+
+        // GET: Planets/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var planet = await _context.Planets.FindAsync(id);
+            if (planet == null)
+            {
+                return NotFound();
+            }
+            return View(planet);
+        }
+
+        // POST: Planets/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name")] Planet planet)
+        {
+            if (id != planet.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(planet);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlanetExists(planet.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(planet);
+        }
+
+        // GET: Planets/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var planet = await _context.Planets
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (planet == null)
+            {
+                return NotFound();
+            }
+
+            return View(planet);
+        }
+
+        // POST: Planets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var planet = await _context.Planets.FindAsync(id);
+            if (planet != null)
+            {
+                _context.Planets.Remove(planet);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlanetExists(Guid id)
+        {
+            return _context.Planets.Any(e => e.Id == id);
+        }
+    }
+}
