cf23f8f402001443eeca4b3829dcdd2715203fed
Vojtěch Lengál <vojta.lengal@gmail.com>
2025-10-16 21:05:09 +0200

    initial commit

---
+++ Planets/Planets/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Planets.Models;
+
+namespace Planets.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Planet> Planets { get; set; }
+    }
+}
